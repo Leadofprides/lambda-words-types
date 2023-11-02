cf827e232f35377f1a7deea2237dbb8336182362
Vlad Narmaniia <vladislav.narmaniia@ibm.com>
2023-11-03 00:40:16 +0100

    implemented integration tests

--- src/words-types/words-types.controller.ts
+++ src/words-types/words-types.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { WordsTypesService } from './words-types.service';
 
 @Controller('words-types')
@@ -6,6 +6,7 @@ export class WordsTypesController {
   constructor(private readonly wordsTypesService: WordsTypesService) {}
 
   @Post()
+  @HttpCode(200)
   async defineTypes(@Body('text') text: string): Promise<Map<string, number>> {
     return this.wordsTypesService.defineTypes(text);
   }
